359ba0fa31aed14c64cf94e937411621b47f9f3a
ysabelchen <63089835+ysabelchen@users.noreply.github.com>
2024-12-05 15:20:06 -0800

    Update dual_graph.py
    Reprojected shapefile to projected coordinate system (3310 = California Albers) to mitigate warnings.

--- dual_graph.py
+++ dual_graph.py
@@ -1,18 +1,34 @@
+from pathlib import Path
+import pandas as pd
 import geopandas as gpd
 from gerrychain import Graph
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# geodata = gpd.read_file("Downloads/ersp data/synced_cbg_pop_and_voting_2020_shapefile/cbg_pop_and_voting_2020.shp")
-geodata = gpd.read_file("cbg_pop_and_voting_2020.shp")
-print("read file")
+# gdf = gpd.read_file("Downloads/ersp data/shapefiles/synced_cbg_pop_and_voting_2020_shapefile/cbg_pop_and_voting_2020.shp")
+gdf = gpd.read_file("cbg_pop_and_voting_2020.shp") # synced shapefile generated using script from sync-cbg-shape-with-voting repo
 
-dual_graph = Graph.from_geodataframe(geodata, adjacency="queen")
+## attempting to combine all shapefiles (water and pop&voting) into one
+# folder = Path("/Users/22ysabelc/Downloads/ersp data/shapefiles")
+# shapefile_paths = list(folder.glob("*/*.shp"))
+# shapefiles = []
+# for shp in shapefile_paths: 
+#     shp_file = gpd.read_file(shp)
+#     shp_file = shp_file.to_crs(3310)
+#     shapefiles.append(shp_file)
+#     print(shp_file.crs)
+# gdf = pd.concat(shapefiles, ignore_index=True).pipe(gpd.GeoDataFrame)
+# gdf.to_file(folder / 'compiled.shp')
+
+print("read files")
+gdf = gdf.to_crs(3310) # reproject to projected coordinate system (3310 = California Albers)
+
+dual_graph = Graph.from_geodataframe(gdf, adjacency="queen")
 dual_graph.to_json("dual_graph.json")
 
 print(len(dual_graph.nodes)) # 25607 CBGs
 
 positions = {node: (row.geometry.centroid.x, row.geometry.centroid.y) 
-             for node, row in geodata.iterrows()}
+             for node, row in gdf.iterrows()}
 nx.draw(dual_graph, pos=positions, node_size=10, edge_color="blue")
 plt.show()
