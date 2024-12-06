4d6539cdb5caad973c6bc2eccd8cfdfd3db2debb
22ysabelc <63089835+22ysabelc@users.noreply.github.com>
2024-12-06 10:57:57 -0800

    updated pathnames and dual-graph.json file

--- generate-dual-graph.py
+++ generate-dual-graph.py
@@ -1,5 +1,6 @@
 # 2019 cbg map to use for comparison: https://databasin.org/datasets/b6359a64b2fa4d19a8b38ff0c348f2d1/ 
 
+import os
 from pathlib import Path
 import pandas as pd
 import geopandas as gpd
@@ -7,9 +8,8 @@ from gerrychain import Graph
 import networkx as nx
 import matplotlib.pyplot as plt
 
-## Read in new data file without water CBGs
-# gdf = gpd.read_file("Downloads/ersp data/shapefiles/synced_cbg_pop_voting_withoutwater_2020/synced_cbg_pop_voting_withoutwater_2020.shp")
-gdf = gpd.read_file("synced_cbg_pop_voting_withoutwater_2020.shp") # synced shapefile generated using script from sync-cbg-shape-with-voting repo
+# Read in new data file without water CBGs
+gdf = gpd.read_file("synced_cbg_pop_voting_withoutwater_2020/synced_cbg_pop_voting_withoutwater_2020.shp") # synced shapefile generated using script from sync-cbg-shape-with-voting repo
 
 gdf = gdf.to_crs(3310) # Reproject to projected coordinate system (3310 = California Albers)
 
@@ -22,3 +22,7 @@ positions = {node: (row.geometry.centroid.x, row.geometry.centroid.y)
              for node, row in gdf.iterrows()}
 nx.draw(dual_graph, pos=positions, node_size=10, edge_color="blue")
 plt.show()
+
+# if not os.path.exists("./dual_graph_withoutwater"):
+#     os.makedirs("./dual_graph_withoutwater")
+# gdf.to_file("./dual_graph_withoutwater.shp", driver="ESRI Shapefile")
--- sync-cbg-shape-with-voting-withoutwater.py
+++ sync-cbg-shape-with-voting-withoutwater.py
@@ -4,8 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from gerrychain import Graph, Partition
 
-shapefile_path = "Downloads/ersp data/tl_2020_06_bg20/tl_2020_06_bg20.shp"
-cbg_path = "Downloads/ersp data/data_voting_population/2020-pres-ca-cbg.csv"
+shapefile_path = "tl_2020_06_bg20/tl_2020_06_bg20.shp"
+cbg_path = "2020-pres-ca-cbg.csv"
 
 gdf = gpd.read_file(shapefile_path)[["GEOID20", "geometry", "STATEFP20", "COUNTYFP20"]]
 cbgdf = pd.read_csv(cbg_path, 
