400c5f458fdda47086479dc3f890fd14b24677e1
ysabelchen <63089835+ysabelchen@users.noreply.github.com>
2024-12-05 22:39:14 -0800

    Update and rename dual_graph.py to generate-dual-graph.py

---
+++ generate-dual-graph.py
@@ -0,0 +1,24 @@
+# 2019 cbg map to use for comparison: https://databasin.org/datasets/b6359a64b2fa4d19a8b38ff0c348f2d1/ 
+
+from pathlib import Path
+import pandas as pd
+import geopandas as gpd
+from gerrychain import Graph
+import networkx as nx
+import matplotlib.pyplot as plt
+
+## Read in new data file without water CBGs
+# gdf = gpd.read_file("Downloads/ersp data/shapefiles/synced_cbg_pop_voting_withoutwater_2020/synced_cbg_pop_voting_withoutwater_2020.shp")
+gdf = gpd.read_file("synced_cbg_pop_voting_withoutwater_2020.shp") # synced shapefile generated using script from sync-cbg-shape-with-voting repo
+
+gdf = gdf.to_crs(3310) # Reproject to projected coordinate system (3310 = California Albers)
+
+dual_graph = Graph.from_geodataframe(gdf, adjacency="queen")
+dual_graph.to_json("dual_graph.json")
+
+print(len(dual_graph.nodes)) # 25584 CBGs that are non-water (of the 25607 CBGs total in California)
+
+positions = {node: (row.geometry.centroid.x, row.geometry.centroid.y) 
+             for node, row in gdf.iterrows()}
+nx.draw(dual_graph, pos=positions, node_size=10, edge_color="blue")
+plt.show()
