18c05b68450b30d031265f3176a28514207c93dd
22ysabelc <63089835+22ysabelc@users.noreply.github.com>
2025-01-24 16:19:57 -0800

    updated generate-dual-graph

--- generate-dual-graph.py
+++ generate-dual-graph.py
@@ -16,23 +16,24 @@ graph = Graph.from_geodataframe(gdf, ignore_errors=False)
 
 print(len(graph.nodes)) # 25584 CBGs that are non-water (of the 25607 CBGs total in California)
 
-island_geoids = ["060759804011", "060839801001", "061110036181", "061119800001", "060375991002", "060375991001", "060375990001", "060375990002", "060375990003", "060375990004"] # retrieved using mapshaper.org, last digit is the block group number
-island_indices = gdf.index[gdf["GEOID20"].isin(island_geoids)].tolist()
-print("Island indices: ", island_indices)
+channel_islands_geoids = ["060839801001", "061110036181", "061119800001", "060375991002", "060375991001", "060375990001", "060375990002", "060375990003", "060375990004"] # retrieved using mapshaper.org, last digit is the block group number
+channel_islands_indices = gdf.index[gdf["GEOID20"].isin(channel_islands_geoids)].tolist()
 
 # Add edges (ferry paths) between farallon islands and pier 39 (geoid: 060750101011)
+farallon_island_geoid = "060759804011"
+farallon_island_index = gdf.index[gdf["GEOID20"] == farallon_island_geoid].tolist()[0]
 pier_39_geoid = "060750101011"
 pier_39_index = gdf.index[gdf["GEOID20"] == pier_39_geoid].tolist()[0]
-graph.add_edges_from([(island_indices[0], pier_39_index)])
+graph.add_edges_from([(farallon_island_index, pier_39_index)])
 # Add edges (ferry paths) between channel islands and ventura harbor (geoid: 061110025003)
 ventura_harbor_geoid = "061110025003"
 ventura_harbor_index = gdf.index[gdf["GEOID20"] == ventura_harbor_geoid].tolist()[0]
-for island_index in island_indices[1:]: # channel islands <-> ventura harbor
+for island_index in channel_islands_indices: # channel islands <-> ventura harbor
     graph.add_edges_from([(island_index, ventura_harbor_index)])
 # Add edges (ferry paths) between alcatraz <-> pier 39
-alcatraz_index = 1316
-alcatraz_geoid = gdf.loc[1316, "GEOID20"]
-graph.add_edges_from([(alcatraz_index, pier_39_index)])
+alcatraz_island_geoid = "060750179032"
+alcatraz_island_index = gdf.index[gdf["GEOID20"] == alcatraz_island_geoid].tolist()[0]
+graph.add_edges_from([(alcatraz_island_index, pier_39_index)])
 
 graph.to_json("./dual-graph.json")
 
